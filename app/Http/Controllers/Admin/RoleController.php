5e9b33c27407b2871c1f813c920e1326c1be33ae
Thái Văn Chiến <vanchienn_@vanchienn-2.local>
2023-04-17 01:45:40 +0700

    CRUD role register

---
+++ app/Http/Controllers/Admin/RoleController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function registered()
+    {
+        $users = User::all();
+        return view('admin.register')->with('users', $users);
+    }
+
+    public function registeredit(Request $request, $id)
+    {
+        $users = User::findOrFail($id);
+        return view('admin.register-edit')->with('users', $users);
+    }
+
+    public function registerupdate(Request $request, $id)
+    {
+        $users = User::find($id);
+        $users->name = $request->input('name');
+        $users->usertype = $request->input('usertype');
+        $users->update();
+
+        return redirect('/role-register')->with('status', 'Updated Successfully!');
+    }
+
+    public function registerdelete($id)
+    {
+        $users = User::findOrFail($id);
+        $users->delete();
+        return redirect('/role-register')->with('status', 'Deleted Successfully!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,34 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth', 'admin']], function(){
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('/role-register', 'App\Http\Controllers\Admin\RoleController@registered');
+
+    Route::get('/role-edit/{id}', 'App\Http\Controllers\Admin\DashboardController@registeredit');
+
+    Route::put('/role-register-update/{id}', 'App\Http\Controllers\Admin\DashboardController@registerupdate');
+
+    Route::delete('/role-delete/{id}', 'App\Http\Controllers\Admin\DashboardController@registerdelete');
+
+    Route::get('/news','App\Http\Controllers\Admin\NewsController@index');
+
+
+    // Route::get('/brands','App\Http\Controllers\Admin\BrandController@index');
+
+    // Route::post('/add-brands','App\Http\Controllers\Admin\BrandController@store');
+
+    // Route::get('/brands-edit/{id}', 'App\Http\Controllers\Admin\BrandController@brandedit');
+
+    // Route::post('/brands-update/{id}', 'App\Http\Controllers\Admin\BrandController@brandupdate');
+
+    // Route::delete('/brands-delete/{id}', 'App\Http\Controllers\Admin\BrandController@branddelete');
+
+
+});
+
+
