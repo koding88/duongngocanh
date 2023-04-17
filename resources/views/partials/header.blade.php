7b10ef64b2b6daf32590ea6a5afdb9e4ca929669
koding88 <duongngocanh2k03@gmail.com>
2023-04-17 17:59:10 +0700

    Add contact to header and active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,21 +9,21 @@
             <!-- Nav -->
             <nav class="nav">
                 <ul>
-                    <li class="active">
+                    <li class="{{ request()-> is('index') ? 'active' : '' }}">
                         <a href="/index">Home</a>
                     </li>
-                    <li>
+                    <li {{ request()-> is('about') ? 'active' : '' }}>
                         <a href="./about.html">About</a>
                     </li>
-                    <li>
+                    <li {{ request()-> is('news') ? 'active' : '' }}>
                         <a href="./news.html">News</a>
                     </li>
-                    <li>
+                    <li {{ request()-> is('shop') ? 'active' : '' }}>
                         <a href="./shop.html">Shop</a>
                     </li>
-                    <!-- <li>
+                    <li {{ request()-> is('contact') ? 'active' : '' }}>
                         <a href="">Contact</a>
-                    </li> -->
+                    </li>
                 </ul>
             </nav>
 
@@ -35,7 +35,7 @@
                 <a href="./checkout.html" class="icon">
                     <i class="fa-solid fa-cart-shopping"></i>
                 </a>
-                <a href="" class="icon">
+                <a href="home" class="icon">
                     <i class="fa-solid fa-user"></i>
                 </a>
             </div>
