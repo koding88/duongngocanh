9c9d602e28632fdbbb7463ca3bc98d42787adcae
koding88 <duongngocanh2k03@gmail.com>
2023-04-17 18:53:57 +0700

    Create about page

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -1,4 +1,11 @@
-<div class="hero-info">
-    <p class="desc">{{ $desc }}</p>
-    <h1 class="heading">{{ $heading }}</h1>
+<!-- Hero-about -->
+<div class="hero-about">
+    <div class="main-content">
+        <div class="body">
+            <div class="hero-info">
+                <p class="desc">{{ $desc }}</p>
+                <h1 class="heading">{{ $heading }}</h1>
+            </div>
+        </div>
+    </div>
 </div>
