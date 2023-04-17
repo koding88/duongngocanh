066883641033db769219f0abd42422cca7482606
koding88 <duongngocanh2k03@gmail.com>
2023-04-18 00:52:03 +0700

    create pagination function for shop

--- resources/views/pages/shop.blade.php
+++ resources/views/pages/shop.blade.php
@@ -29,151 +29,42 @@
                 </div>
 
                 <div class="product-list">
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <a href="./detail.html">
-                            <img
-                                src="https://images.unsplash.com/photo-1553456558-aff63285bdd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80g"
-                                alt="Pepsi"
-                                class="thumb"
-                            />
-                        </a>
-                        <div class="info">
-                            <h3 class="title">
-                                <a href="./detail.html"> Pepsi </a>
-                            </h3>
-                            <p class="category">Water</p>
-                            <span class="price">85$</span>
-                            <a href="#!" class="btn btn-cart">
-                                <i class="fas fa-shopping-cart"></i>
-                                Add to Cart
-                            </a>
-                        </div>
-                    </div>
+                    @php
+                        $itemsPerPage = 9;
+                        $totalItems = 27;
+                        $totalPages = ceil($totalItems / $itemsPerPage);
+                        $currentPage = request()->get('page', 1);
+                        $currentPage = max(1, min($currentPage, $totalPages));
+                        $startIndex = ($currentPage - 1) * $itemsPerPage;
+                    @endphp
+                    {{-- Product items --}}
+                    @if (request()->is('shop'))
+                        @for ($i = $startIndex; $i < $startIndex + $itemsPerPage && $i < $totalItems; $i++)
+                            @include('components.product')
+                        @endfor
+                    @else
+                        {{-- Default items --}}
+                        @include('components.product')
+                    @endif
                 </div>
 
                 <ul class="pagination">
                     <li>
-                        <a href="#!">
+                        <a href="{{ $currentPage == 1 ? '#' : '?page=' . ($currentPage - 1) }}">
                             <i class="fa-solid fa-arrow-left"></i>
                             Prev
                         </a>
                     </li>
+
+                    @for ($i = 1; $i <= $totalPages; $i++)
+                        <li>
+                            <a href="?page={{ $i }}"
+                                class="{{ $i == $currentPage ? 'active' : '' }}">{{ $i }}</a>
+                        </li>
+                    @endfor
+
                     <li>
-                        <a href="#!" class="active">1</a>
-                    </li>
-                    <li>
-                        <a href="#!">2</a>
-                    </li>
-                    <li>
-                        <a href="#!">3</a>
-                    </li>
-                    <li>
-                        <a href="#!">
+                        <a href="{{ $currentPage == $totalPages ? '#' : '?page=' . ($currentPage + 1) }}">
                             Next
                             <i class="fa-solid fa-arrow-right"></i>
                         </a>
