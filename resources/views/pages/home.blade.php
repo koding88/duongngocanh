dfb7a8bcc4d4d8662ef52590ae0eca044ea8cd83
koding88 <duongngocanh2k03@gmail.com>
2023-04-17 18:56:29 +0700

    Add components discount

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -274,26 +274,8 @@
         </div>
     </div>
 
-    <!-- Discount -->
-    <div class="discount">
-        <div class="main-content">
-            <div class="body">
-                <h3 class="title">
-                    December sale is on! with big
-                    <strong class="highlight">Discount...</strong>
-                </h3>
-                <div class="discount-info">
-                    <p class="desc">
-                        Sale! <br />
-                        Upto
-                    </p>
-                    <strong class="highlight">50%</strong>
-                    <p class="desc">off</p>
-                </div>
-                <a href="#!" class="btn btn-cart">Shop now</a>
-            </div>
-        </div>
-    </div>
+    {{-- Discount --}}
+    @include('components.discount')
 
     <!-- News -->
     <div class="news">
