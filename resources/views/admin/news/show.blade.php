802f2faabbb6ed958630eb921c206c3ef8e41640
Quynh Nga <128832470+qnga5523@users.noreply.github.com>
2023-04-19 14:21:08 +0700

    new

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -19,7 +19,7 @@
                         <div class="col-md-12 col-sm-12">
                             <div class="form-group">
                                 <strong>Image:</strong>
-                                <img src="/public/images/news/{{$news->image}}" width="250px">
+                                <img src="/images{{$news->image}}" width="250px">
                             </div>
                         </div>                        
                             <div class="row">
